cf3f5fc7d9bc9743fe36385e8701f52ad6706a9d
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-20 21:05:59 -0600

    Continuación de flutter-formvalidation

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_formvalidation/src/bloc/provider.dart';
+import 'package:flutter_formvalidation/src/pages/home_page.dart';
+import 'package:flutter_formvalidation/src/pages/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      child: MaterialApp(
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple
+        ),
+      )
+    );
+  }
+}
