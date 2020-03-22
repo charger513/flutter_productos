ca57e673814485d7aa3e3b4593adcd51027cbae2
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-21 18:34:46 -0600

    Pruebas de carga de fotografías y actualizar registros en Firebase

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -124,12 +124,16 @@ class _ProductoPageState extends State<ProductoPage> {
     );
   }
 
-  void _submit() {
+  void _submit() async {
     if(!formKey.currentState.validate()) return;
 
     formKey.currentState.save();
 
     setState(() { _guardando = true; });
+
+    if(foto != null) {
+      producto.fotoUrl = await productoProvider.subirImagen(foto);
+    }
 
     if(producto.id == null) {
       productoProvider.crearProducto(producto);
@@ -156,8 +160,9 @@ class _ProductoPageState extends State<ProductoPage> {
 
   Widget _mostrarFoto() {
     if(producto.fotoUrl != null) {
-      return Container(
-        child: Center(child: Text('Foto URL'))
+      return FadeInImage(
+        image: NetworkImage(producto.fotoUrl),
+        placeholder: AssetImage('assets/img/jar-loading.gif')
       );
     } else {
       if( foto != null ){
